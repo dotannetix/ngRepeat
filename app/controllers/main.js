9b79dd59a3a8c134e90a3ca77b2d0c24071f042a
Dotan <dotan@netix.co.il>
2017-04-14 17:03:25 +0300

    ng-model

---
+++ app/controllers/main.js
@@ -0,0 +1,20 @@
+(function () {
+    var app = angular.module("app");
+    app.controller("main", main)
+
+    function main() {
+        var vm = this;
+        vm.colorsBox = ""
+        vm.title = "hello from main"
+        
+//        var result =document.getElementById("result");
+//        var text1= document.getElementById("text1");
+//        text1.onkeyup =function(event){
+//           // console.log(event.key)
+//          result.innerHTML="hello " +text1.value;
+//        }
+        
+        
+     
+    }
+})();
